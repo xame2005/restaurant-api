60d4a75bb92a53a096f8e859b3563128764a8653
Samuel Gonzalez <xame2005@gmail.com>
2021-12-05 16:23:36 -0600

    APPS created

--- restaurant_project/restaurant_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RestaurantView
-
-urlpatterns = [
-    path('restaurants/', RestaurantView.as_view(), name='restaurants_list'),
-    path('restaurants/<int:id>', RestaurantView.as_view(), name='restaurants_process'),
-]
