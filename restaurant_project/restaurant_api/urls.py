c6f36b5dc320f2c992a9cd48a91c5254e87ed6b2
Samuel Gonzalez <xame2005@gmail.com>
2021-12-05 15:26:13 -0600

    [CREATED] Project APPS

--- restaurant_project/restaurant_api/urls.py
+++ restaurant_project/restaurant_api/urls.py
@@ -3,6 +3,5 @@ from .views import RestaurantView
 
 urlpatterns = [
     path('restaurants/', RestaurantView.as_view(), name='restaurants_list'),
-    path('restaurants/<int:id>', RestaurantView.as_view(),
-         name='restaurants_process'),
+    path('restaurants/<int:id>', RestaurantView.as_view(), name='restaurants_process'),
 ]
--- restaurant_project/restaurant_project/settings.py
+++ restaurant_project/restaurant_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant_api',
+    'orders',
+    'menu',
+    'ticket',
 ]
 
 MIDDLEWARE = [
